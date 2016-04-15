b9f68279b2df619d8e2d94393df1f9f393a64a13
anasmohammad <anasaiub@gmail.com>
2016-04-15 20:01:54 +0600

    initial commit with underscores wp frame

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Startdev
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'startdev' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'startdev' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'startdev' ), 'startdev', '<a href="http://mohammadanas.me" rel="designer">Mohammad Anas</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
